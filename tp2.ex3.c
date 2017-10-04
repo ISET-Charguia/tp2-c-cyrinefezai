82c811e86ab806018f93621b7ff177a24e4a4eaa
cyrine fezai <32401480+cyrinefezai@users.noreply.github.com>
2017-10-04 12:47:27 +0100

    Add files via upload

---
+++ tp2.ex3.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main() {
+int a,b ;
+double x ;
+printf("donner le premier entier");
+scanf ("%d",&a);
+printf("donner la deuxieme entier");
+scanf("%d",&b);
+x=-b/a ;
+printf("%lf x",x);
+	return 0;
+}
