65491abc89dea776fb2ac479111867e98ef0fb81
cyrine fezai <32401480+cyrinefezai@users.noreply.github.com>
2017-10-04 12:46:29 +0100

    Add files via upload

---
+++ tp2.ex5.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main() {
+int x,c,d,u ;
+printf("donner un entier ");
+scanf("%d",&x);
+c== x / 100 ;
+d== (x % 100)/10;
+u==x %10 ;
+printf("%d,%d,%d",c,d,u);
+	return 0;
+}
