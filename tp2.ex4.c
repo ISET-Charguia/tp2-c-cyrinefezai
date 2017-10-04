9aeb401eafb22001e7106ca01d482c47c0754106
cyrine fezai <32401480+cyrinefezai@users.noreply.github.com>
2017-10-04 12:47:01 +0100

    Add files via upload

---
+++ tp2.ex4.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main(void) {
+int a;
+printf("donner l'année");
+scanf("%d",&a);
+if ('a %4 =0'){
+	printf("l'année %d est bissextile");
+}else
+	printf("l'année %d n'est pas bissextile");
+	return 0;
+}
